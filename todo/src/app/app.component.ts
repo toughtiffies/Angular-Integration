1d82c908d4735a475f88e92e4d1b34fd07195546
toughtiffies <tifpar7029@students.ecpi.edu>
2025-03-13 10:23:07 -0400

    First Commit

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from "@angular/core"; // Add this line
+import { Item } from "./item";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title = "todo";
+  filter: "all" | "active" | "done" = "all";
+  allItems: Item[] = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+}
